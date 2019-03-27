a88851b31cdb40cb9de3482a1536a5feaa88b165
Jairo <jairotft@gmail.com>
2019-03-26 22:28:05 -0500

    Configuracion de las aplicaciones en settings, definicion de modelos y migraciones

--- seekerpet/mascotas/models.py
+++ seekerpet/mascotas/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Estado(models.Model):
+    codigo = models.CharField(max_length=255, null=False, blank=True)
+    nombre = models.CharField(max_length=255, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados'
+
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Mascota(models.Model):
+    codigo = models.CharField(max_length=255, null=False, blank=True)
+    nombre = models.CharField(max_length=255, null=False, blank=False)
+    descripcion = models.TextField(null=True, blank=True)
+    imagen = models.ImageField(null=False, blank=False)
+    lugar_perdida = models.TextField(null=False, blank=False)
+    fecha_perdida = models.DateField(null=False, blank=False)
+    propietario = models.ForeignKey(User, null=False, blank=True, on_delete=models.CASCADE, related_name='mascotas')
+    estado = models.ForeignKey(Estado, null=False, blank=True, on_delete=models.CASCADE, related_name='mascotas')
+
+    class Meta:
+        verbose_name = 'Mascota'
+        verbose_name_plural = 'Mascotas'
+
+    def __str__(self):
+        return str(self.nombre)
--- seekerpet/seguridades/models.py
+++ seekerpet/seguridades/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ciudad(models.Model):
+    codigo = models.CharField(max_length=255, null=False, blank=True)
+    nombre = models.CharField(max_length=255, null=False, blank=False)
+
+    class Meta:
+        verbose_name = 'Ciudad'
+        verbose_name_plural = 'Ciudades'
+    
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Perfil(models.Model):
+    telefono = models.CharField(max_length=13, null=False, blank=False)
+    direccion = models.TextField(null=True, blank=True)
+    user = models.OneToOneField(User, null=False, blank=True, on_delete=models.CASCADE, related_name='perfil')
+    ciudad = models.ForeignKey(Ciudad, null=False, blank=False, on_delete=models.CASCADE, related_name='perfiles')
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+
+    def __str__(self):
+        return str(self.user)
